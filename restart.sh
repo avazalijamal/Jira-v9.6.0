06f3c38eea3cfba6f7dd5f438bff1bd9dec9814f
avazalijamal <avazalijamal@gmail.com>
2024-07-03 16:36:58 +0400

    Version 1.3.9

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
 echo "Addım 1: Jira-nın Restart Edilir"
 printf "\n"
 cd /opt/atlassian/jira/bin/
-sudo ./start-jira.sh -fg
-sudo ./stop-jira.sh -fg
-sudo ./start-jira.sh -fg
+sudo ./start-jira.sh
+sudo ./stop-jira.sh
+sudo ./start-jira.sh
 printf "\n"
 echo "Jira-nın Restart Edildi"
 printf "\n"
